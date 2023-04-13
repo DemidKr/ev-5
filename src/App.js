8f9fcdfd77710828b76781b6a00c555a966614d2
DemidKr <demid.krasnovidov@gmail.com>
2023-04-13 13:19:29 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Grid,
+} from "@mui/material";
+import React, {useState} from "react";
 
 function App() {
+
+
+  const rand = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            p: 1,
+            m: 1,
+            bgcolor: 'background.paper',
+            borderRadius: 1,
+          }}
+      >
+        <Grid item lg={12} sx={{mb: 2}}>
+
+        </Grid>
+      </Box>
   );
 }
 
